5ba6692c0899ef6ed18ab71de8d0a019c134ae3a
sideeq12 <abdwaheed2018@gmail.com>
2020-11-18 02:46:22 -0800

    moster rolodex backbone with fetch API

---
+++ src/components/wrappers/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state= {
+            monsters : []
+        }
+            
+    }
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({monsters : users}))
+    }
+
+    render(){
+        return(
+            <div className="App">
+        {this.state.monsters.map(monster =>(
+         <h2 key={monster.id} >{monster.name}</h2>
+         )
+         )}
+        </div>
+        )
+    }
+
+}
+export default App;
